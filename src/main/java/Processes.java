9888445b0dd58771a739e7cd8e3394242c736831
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2018-10-21 22:20:04 -0700

    Finished userLogin method, further testing needed to check for bugs.

--- src/main/java/Processes.java
+++ src/main/java/Processes.java
@@ -17,13 +17,15 @@ public class Processes {
         return true;
     }
 
+    //method used for user login. returns a null cookie if login failed otherwise a valid cookie is returned
     public static Cookie userLogin(String username, String password) {
-        //change length variable to the number of users in the database
-        int length = 50;
-        for (int i = 0; i < length; i++) {
-            //check all the database for the username and compare the passwords
-            //if passwords match return the cookie else break out of the loop
+        DataBase d = DataBase.getInstance();
+        String token = Long.toString(System.nanoTime());
+        Cookie authentication = new Cookie("token", token);
+        if (!(d.loginCheck(username, password))) {
+            return null;
+        } else {
+            return authentication;
         }
-       return null;
     }
 }
